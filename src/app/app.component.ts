e04c0b499b10a78cd0488e37e6cba3d2db0f7467
Bogdan Balc <bogdan.balc@gmail.com>
2020-02-08 10:32:40 +0200

    code review changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,25 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  title = "learn Angular";
 
-  title = 'learn Angular';
+  animals = [
+    /*{id: 1, name: 'dog'},*/
+    { id: 1, name: "cat" }
+  ];
 
-  animalinput: string;
-  plantinput: string;
-  carinput: string;
+  plants = [
+    { id: 1, name: "flower" },
+    { id: 2, name: "tree" }
+  ];
 
-  animals = [/*{id: 1, name: 'dog'},*/
-    {id: 1, name: 'cat'}];
-
-  plants = [{id: 1, name: 'flower'},
-    {id: 2, name: 'tree'}];
-
-  cars = [{id: 1, name: 'racecar'},
-    {id: 2, name: 'sedan'}];
-
-  addAnimals() {
-    // this.animals.push({id: this.animals.length + 1, name: 'dog'});
-
-    this.animals.push({id: this.animals.length + 1, name: this.animalinput});
-    this.animalinput = '';
-  }
-
-  removeAnimals() {
-    // alert(this.animals.length);
-    if (this.animals.length > 0) {
-      this.animals.splice(this.animals.length - 1, 1);
-    }
-  }
-
-  addPlants() {
-    // this.plants.push({id: this.plants.length + 1, name: 'flower'});
-    this.plants.push({id: this.plants.length + 1, name: this.plantinput});
-    this.plantinput = '';
-  }
-
-  removePlants() {
-    if (this.plants.length > 0) {
-      this.plants.splice(this.plants.length - 1, 1);
-    }
-  }
-
-  addCars() {
-    // this.cars.push({id: this.cars.length + 1, name: 'sportscar'});
-    this.cars.push({id: this.cars.length + 1, name: this.carinput});
-    this.carinput = '';
-  }
-
-  removeCars() {
-    if (this.cars.length > 0) {
-      this.cars.splice(this.cars.length - 1, 1);
-    }
-  }
-
+  cars = [
+    { id: 1, name: "racecar" },
+    { id: 2, name: "sedan" }
+  ];
 }
--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,46 +1,56 @@
-import {Component, OnInit, OnChanges, Input} from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-dog',
-  templateUrl: './dog.component.html',
-  styleUrls: ['./dog.component.css']
+  selector: "app-dog",
+  templateUrl: "./dog.component.html",
+  styleUrls: ["./dog.component.css"]
 })
 export class DogComponent implements OnInit, OnChanges {
   @Input() items;
   dogsFledTheScene;
-  title = 'show/hide';
+  title = "show/hide";
 
   isActive = true;
 
-  constructor() {
-  }
+  input: string;
+
+  constructor() {}
 
   ngOnInit() {
     this.dogsFledTheScene = this.items.length < 2 && this.isActive;
-    console.log('test text', this.dogsFledTheScene);
+    console.log("test text", this.dogsFledTheScene);
   }
 
   ngOnChanges() {
     this.dogsFledTheScene = this.items.length < 2 && this.isActive;
-    console.log('mesaj', this.items.length);
+    console.log("mesaj", this.items.length);
   }
 
   // tslint:disable-next-line:use-lifecycle-interface
   ngDoCheck() {
     this.dogsFledTheScene = this.items.length < 2 && this.isActive;
-    console.log('mesaj2', this.items.length);
+    console.log("mesaj2", this.items.length);
   }
-
 
   handleClickEvent() {
     // console.log("Click me! button was clicked");
     this.isActive = !this.isActive;
     if (this.isActive) {
-      this.title = 'hide';
+      this.title = "hide";
     } else {
-      this.title = 'show';
+      this.title = "show";
     }
     this.dogsFledTheScene = this.items.length < 2 && this.isActive;
   }
 
+  addItems() {
+    this.items.push({ id: this.items.length + 1, name: this.input });
+    this.input = "";
+  }
+
+  removeItems() {
+    if (this.items.length > 0) {
+      this.items.splice(this.items.length - 1, 1);
+    }
+  }
 }
